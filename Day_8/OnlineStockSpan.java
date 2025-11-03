736b8e157707f5f458b3b0420f60adec2c7e66b5
Ayush Chaudhary <147637466+ayush23chaudhary@users.noreply.github.com>
2025-11-03 16:32:08 +0530

    Implement OnlineStockSpan with StockSpanner class

---
+++ Day_8/OnlineStockSpan.java
@@ -0,0 +1,33 @@
+package Day_8;
+
+import java.util.Stack;
+
+public class OnlineStockSpan {
+
+	public static void main(String[] args) {
+
+	}
+
+	class StockSpanner {
+
+		private Stack<int[]> stack;
+
+		public StockSpanner() {
+			stack = new Stack<>();
+		}
+
+		public int next(int price) {
+			int span = 1;
+			while (!stack.isEmpty() && stack.peek()[0] <= price) {
+				span += stack.pop()[1]; // Add popped span
+			}
+			stack.push(new int[] { price, span }); // Store price and span
+			return span;
+		}
+	}
+
+	/**
+	 * Your StockSpanner object will be instantiated and called as such:
+	 * StockSpanner obj = new StockSpanner(); int param_1 = obj.next(price);
+	 */
+}
